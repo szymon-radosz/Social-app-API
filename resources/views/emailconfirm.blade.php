bcb8eaf822f3f4693cf33e7c7968a50b1dacac0e
RadoszSzymon <radoszszymon@gmail.com>
2019-02-23 16:25:48 +0100

    user register confirmation

--- resources/views/emailconfirm.blade.php
+++ resources/views/emailconfirm.blade.php
@@ -6,7 +6,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Registration Confirmed</div>
                 <div class="panel-body">
-                Your Email is successfully verified. Click here to <a href="{{url(‘/login’)}}">login</a>
+                Your Email is successfully verified. Click here to <a href="{{url('/login')}}">login</a>
                 </div>
             </div>
         </div>
