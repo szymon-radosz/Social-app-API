cf88d7335ffc53e9e5de4a4039d33f48cf9bcb62
RadoszSzymon <radoszszymon@gmail.com>
2019-08-17 07:16:22 +0200

    reset password - password type inputs styles

--- resources/views/resetPassword/reset.blade.php
+++ resources/views/resetPassword/reset.blade.php
@@ -55,13 +55,13 @@
                                 {{ csrf_field() }}
 
                                 <div class="form-group">
-                                    {!! Form::label('password', 'Nowe hasło') !!}
-                                    {!! Form::password('password', null, ['class' => 'form-control']) !!}
+                                    <div>{!! Form::label('password', 'Nowe hasło') !!}</div>
+                                    {{ Form::password('password', array('id' => 'password', "class" => "form-control", "autocomplete" => "off)) }}
                                 </div>
 
                                 <div class="form-group">
-                                    {!! Form::label('passwordConfirmation', 'Potwierdzenie hasła') !!}
-                                    {!! Form::password('passwordConfirmation', null, ['class' => 'form-control']) !!}
+                                    <div>{!! Form::label('passwordConfirmation', 'Potwierdzenie hasła') !!}</div>
+                                    {{ Form::password('passwordConfirmation', array('id' => 'passwordConfirmation', "class" => "form-control", "autocomplete" => "off)) }}}
                                 </div>
 
                                 {!! Form::submit('Zmień hasło', ['class' => 'btn resetBtn']) !!}
